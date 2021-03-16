22c2f8bed37084db2a9e0e5891b305871ffd3419
JLabCISSC <JLabCISSC@gmail.com>
2021-03-16 19:35:56 +0000

    fixed

--- src/main/java/works/weave/socks/cart/controllers/HealthCheckController.java
+++ src/main/java/works/weave/socks/cart/controllers/HealthCheckController.java
@@ -29,13 +29,16 @@ public class HealthCheckController {
        Date dateNow = Calendar.getInstance().getTime();
        HealthCheck app = new HealthCheck("carts", "OK", dateNow);
        HealthCheck database = new HealthCheck("carts-db", "OK", dateNow);
+
        try {
           mongoTemplate.executeCommand("{ buildInfo: 1 }");
        } catch (Exception e) {
           database.setStatus("err");
        }
+
        healthChecks.add(app);
        healthChecks.add(database);
+
        map.put("health", healthChecks);
        return map;
     }
